fc849d79640ad1b865bf9d136e06e4016581aaf4
Ameer Abdulaziz <ameer.abdulaziz93@gmail.com>
2024-01-11 23:59:45 +0200

    Start `backend` project part & Start `contacts` app & Add `Contact` model

---
+++ backend/contacts/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='contacts_created')
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='contacts_updated')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.phone}'
