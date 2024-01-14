f00a211496a28575fc3467836f402e92c343f615
Ameer Abdulaziz <ameer.abdulaziz93@gmail.com>
2024-01-14 22:24:26 +0200

    Add authentication on the home page and redirect to login if not authenticated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import Header from "./components/Header";
+import PrivateRoutes from "./utils/PrivateRoutes";
 
 function App() {
     return (
         <Router>
             <Header />
             <Routes>
-                <Route element={<HomePage />} path="/" />
+                <Route element={<PrivateRoutes />}>
+                    <Route element={<HomePage />} path="/" />
+                </Route>
                 <Route element={<LoginPage />} path="/login" />
             </Routes>
         </Router>
