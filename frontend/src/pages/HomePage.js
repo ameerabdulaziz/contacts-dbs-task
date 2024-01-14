f91ad77d77158d5fea2d72d6d1846c14ce323dd6
Ameer Abdulaziz <ameer.abdulaziz93@gmail.com>
2024-01-15 01:27:13 +0200

    Show contacts info on the home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,12 +1,39 @@
-import React, { useContext } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import AuthContext from "../context/AuthContext";
 
 const HomePage = () => {
+    let [contacts, setContacts] = useState([])
+    let {authTokens, logoutUser} = useContext(AuthContext)
+
+    useEffect(()=> {
+        getContacts()
+    }, [])
+
+
+    let getContacts = async() =>{
+        let response = await fetch('http://127.0.0.1:8000/api/contacts/', {
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':'Bearer ' + String(authTokens.access)
+            }
+        })
+        let data = await response.json()
+        if ( response.status === 200 ){
+            setContacts(data.results)
+        } else if( response.statusText === 'Unauthorized'){
+            logoutUser()
+        }
+
+    }
     return (
         <div>
             <h1>Contacts App</h1>
-            <h3>Hello</h3>
-            <p>Your are logged to the home page!</p>
+            <ul>
+                {contacts.map(contact => (
+                    <li key={contacts.indexOf(contact)}>{contact.name} - {contact.email}</li>
+                ))}
+            </ul>
         </div>
     )
 }
